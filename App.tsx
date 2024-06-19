b46c80d6220d27216ccb074a3b81c126e9881add
masaditya <aeka489@gmail.com>
2024-06-19 09:10:13 +0700

    main : fix icon

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import RecordScreen from './src/screens/Record';
 import ActivityScreen from './src/screens/Activity';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,19 +13,17 @@ function App(): React.JSX.Element {
       <Tab.Navigator
         screenOptions={({route}) => ({
           // eslint-disable-next-line react/no-unstable-nested-components
-          tabBarIcon: ({focused, color, size}) => {
+          tabBarIcon: ({color, size}) => {
             let iconName = '';
 
             if (route.name === 'Record') {
-              iconName = focused
-                ? 'mic-outline'
-                : 'ios-information-circle-outline';
+              iconName = 'mic';
             } else if (route.name === 'Activity') {
-              iconName = focused ? 'ios-list' : 'ios-list-outline';
+              iconName = 'grid';
             }
 
             // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
+            return <FeatherIcon name={iconName} size={size} color={color} />;
           },
           tabBarActiveTintColor: '#1abc9c',
           tabBarInactiveTintColor: '#34495e',
